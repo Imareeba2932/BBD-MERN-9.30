fe429f58c8ead24377de3ddf7d0f7a1903d5aa71
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-03-28 09:57:56 +0530

    routers

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/add', (req, res) => {
+    res.send('Hello World!')
+})
+
+
 app.listen(port, () => {
     console.log('Server is running at the port 3000')
 })
